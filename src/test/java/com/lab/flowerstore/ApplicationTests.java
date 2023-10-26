0765c30c9aebe1a3b3714e8e61fdc3ca1f793cb9
khrystynamk <khrystyna.mysak@gmail.com>
2023-10-26 23:08:18 +0300

    Last fix

--- src/test/java/com/lab/flowerstore/ApplicationTests.java
+++ src/test/java/com/lab/flowerstore/ApplicationTests.java
@@ -1,12 +1,9 @@
 package com.lab.flowerstore;
 
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
-    @Test
-    void contextLoads() {
-    }
+
 }
