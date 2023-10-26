c201b077cb1294bf25330d1d9646a1812b9f0af9
khrystynamk <khrystyna.mysak@gmail.com>
2023-10-26 23:21:24 +0300

    Experiment

--- src/test/java/com/lab/flowerstore/ApplicationTests.java
+++ src/test/java/com/lab/flowerstore/ApplicationTests.java
@@ -1,9 +1,17 @@
 package com.lab.flowerstore;
 
 
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import com.lab.flowerstore.flower.FlowerRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {FlowerRepository.class})
 class ApplicationTests {
-
+    @Test
+    void contextLoads() {
+    }
 }
